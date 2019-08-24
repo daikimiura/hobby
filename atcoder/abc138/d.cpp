6fe083e42ac27d32f0bf7556d30863a4c73e70fe
daikimiura <daikiii5555@gmail.com>
2019-08-24 19:57:52 +0900

    Add abc137 d

--- atcoder/abc138/d.cpp
+++ atcoder/abc138/d.cpp
@@ -3,4 +3,39 @@
 typedef long long ll;
 using namespace std;
 
-int main() { return 0; }
+vector<int> to[200005];
+vector<int> ans;
+
+void dfs(int v, int p = -1) {
+  for (int u : to[v]) {
+    if (u == p)
+      continue;
+    ans[u] += ans[v];
+    dfs(u, v);
+  }
+}
+
+int main() {
+  int N, Q;
+  cin >> N >> Q;
+
+  rep(i, N - 1) {
+    int a, b;
+    cin >> a >> b;
+    --a, --b;
+    to[a].push_back(b);
+    to[b].push_back(a);
+  }
+  ans.resize(N);
+
+  rep(i, Q) {
+    int p, x;
+    cin >> p >> x;
+    --p;
+    ans[p] += x;
+  }
+
+  dfs(0);
+  rep(i, N) { cout << ans[i] << endl; }
+  return 0;
+}
