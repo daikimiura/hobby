26155352da45927fa64cb1a9d0c4a3104fe847d8
daikimiura <daikiii5555@gmail.com>
2019-08-25 00:26:34 +0900

    Change atcoder template

--- atcoder/template/a.cpp
+++ atcoder/template/a.cpp
@@ -2,5 +2,6 @@
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 typedef long long ll;
 using namespace std;
+long long MOD = 1e9 + 7;
 
 int main() { return 0; }
--- atcoder/template/e.cpp
+++ atcoder/template/e.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 typedef long long ll;
+long long MOD = 1e9 + 7;
 using namespace std;
 
 int main() { return 0; }
