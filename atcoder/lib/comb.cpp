dda18b9ffe36cb57dda2836c44609fc087322ab7
daikimiura <daikiii5555@gmail.com>
2020-02-04 22:32:18 +0900

    Add abc145-d

--- atcoder/lib/comb.cpp
+++ atcoder/lib/comb.cpp
@@ -1,9 +1,3 @@
-#include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-typedef long long ll;
-long long MOD = 1e9 + 7;
-using namespace std;
-
 const int MAX = 1000000;
 long long fac[MAX], finv[MAX], inv[MAX];
 
@@ -26,19 +20,9 @@ long long COM(int n, int k) {
   return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
 }
 
-int main() {
-  int x, y;
-  cin >> x >> y;
-  if ((x + y) % 3 != 0) {
-    cout << 0 << endl;
-    return 0;
-  }
-
-  int n, m;
-  n = (2 * y - x) / 3;
-  m = (2 * x - y) / 3;
-  COMinit();
-  cout << COM(m + n, n) << endl;
-
-  return 0;
-}
+// int main() {
+//   COMinit();
+//   cout << COM(1000, 100) << endl;
+//
+//   return 0;
+// }
