cd0bad045a986380fe752515fd445d06d0939dc9
daikimiura <daikiii5555@gmail.com>
2020-02-16 00:35:15 +0900

    Add abc149 d

--- atcoder/abc/149/d.cpp
+++ atcoder/abc/149/d.cpp
@@ -22,4 +22,49 @@ template <class T> inline bool chmax(T &a, T b) {
   return false;
 }
 
-int main() { return 0; }
+int main() {
+  int n, k;
+  cin >> n >> k;
+  int r, s, p;
+  cin >> r >> s >> p;
+  string t;
+  cin >> t;
+
+  string t_sub[k];
+  rep(i, n) {
+    int mod = i % k;
+    t_sub[mod] += t[i];
+  }
+
+  ll ans = 0;
+
+  rep(i, k) {
+    string t_tmp = t_sub[i];
+
+    // dp[i][j]: i回目にjを出した時にi回目までに得られる最高得点
+    int dp[100010][3];
+    dp[0][0] = 0; // 0: グー, 1: チョキ, 2: パー
+    dp[0][1] = 0; // 0: グー, 1: チョキ, 2: パー
+    dp[0][2] = 0; // 0: グー, 1: チョキ, 2: パー
+    repi(j, 1, t_tmp.size() + 1) {
+      if (t_tmp[j - 1] == 'r') {
+        dp[j][0] = max(dp[j - 1][1], dp[j - 1][2]);
+        dp[j][1] = max(dp[j - 1][2], dp[j - 1][0]);
+        dp[j][2] = max(dp[j - 1][0], dp[j - 1][1]) + p;
+      } else if (t_tmp[j - 1] == 's') {
+        dp[j][0] = max(dp[j - 1][1], dp[j - 1][2]) + r;
+        dp[j][1] = max(dp[j - 1][2], dp[j - 1][0]);
+        dp[j][2] = max(dp[j - 1][0], dp[j - 1][1]);
+      } else if (t_tmp[j - 1] == 'p') {
+        dp[j][0] = max(dp[j - 1][1], dp[j - 1][2]);
+        dp[j][1] = max(dp[j - 1][2], dp[j - 1][0]) + s;
+        dp[j][2] = max(dp[j - 1][0], dp[j - 1][1]);
+      }
+    }
+    ans +=
+        max(dp[t_tmp.size()][0], max(dp[t_tmp.size()][1], dp[t_tmp.size()][2]));
+  }
+
+  cout << ans << endl;
+  return 0;
+}
