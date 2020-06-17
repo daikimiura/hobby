5f092ac16b23fca84e9de52f8ba45253287a0d24
daikimiura <daikiii5555@gmail.com>
2020-06-18 01:10:48 +0900

    Add 12.5 & 12.6

--- rust_practice/minigrep/src/main.rs
+++ rust_practice/minigrep/src/main.rs
@@ -9,12 +9,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("problem parsing arguments: {}", err);
+        eprintln!("problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("application error: {}", e);
+        eprintln!("application error: {}", e);
         process::exit(1);
     }
 }
