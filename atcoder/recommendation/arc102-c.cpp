642341e782f0a793d86e0d89efd0c6d94a651169
daikimiura <daikiii5555@gmail.com>
2020-06-19 03:31:14 +0900

    Add arc102-c

---
+++ atcoder/recommendation/arc102-c.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repi(i, a, b) for (int i = int(a); i < int(b); ++i)
+typedef long long ll;
+long long mod = 1e9 + 7;
+const int iINF = 0x3f3f3f3f;
+const long long INF = 1LL << 60;
+using namespace std;
+
+template <class T>
+inline bool chmin(T &a, T b)
+{
+  if (a > b)
+  {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+inline bool chmax(T &a, T b)
+{
+  if (a < b)
+  {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+int main()
+{
+  int n, k;
+  cin >> n >> k;
+  if (k % 2 == 0)
+  {
+
+    cout << ll(pow(n / k, 3) + pow((n + k / 2) / k, 3)) << endl;
+  }
+  else
+  {
+    cout << ll(pow(n / k, 3)) << endl;
+  }
+  return 0;
+}
