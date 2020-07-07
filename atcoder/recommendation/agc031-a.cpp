c11ef1a513786431eb3683008d04abe32a8713f7
daikimiura <daikiii5555@gmail.com>
2020-07-07 17:17:30 +0900

    Add agc031-a & agc-05-a

---
+++ atcoder/recommendation/agc031-a.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repi(i, a, b) for (int i = int(a); i < int(b); ++i)
+typedef long long ll;
+long long mod = 1e9 + 7;
+const int iINF = 0x3f3f3f3f;
+const long long INF = 1LL << 60;
+using namespace std;
+using pint = pair<int, int>;
+
+template <class T>
+inline bool chmin(T &a, T b)
+{
+  if (a > b)
+  {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+inline bool chmax(T &a, T b)
+{
+  if (a < b)
+  {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+struct mint
+{
+  ll x; // typedef long long ll;
+  mint(ll x = 0) : x((x % mod + mod) % mod) {}
+  mint operator-() const { return mint(-x); }
+  mint &operator+=(const mint a)
+  {
+    if ((x += a.x) >= mod)
+      x -= mod;
+    return *this;
+  }
+  mint &operator-=(const mint a)
+  {
+    if ((x += mod - a.x) >= mod)
+      x -= mod;
+    return *this;
+  }
+  mint &operator*=(const mint a)
+  {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const { return mint(*this) += a; }
+  mint operator-(const mint a) const { return mint(*this) -= a; }
+  mint operator*(const mint a) const { return mint(*this) *= a; }
+  mint pow(ll t) const
+  {
+    if (!t)
+      return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1)
+      a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod - 2); }
+  mint &operator/=(const mint a) { return *this *= a.inv(); }
+  mint operator/(const mint a) const { return mint(*this) /= a; }
+};
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  map<char, int> mp;
+
+  rep(i, n)
+  {
+    char c;
+    cin >> c;
+    mp[c]++;
+  }
+
+  mint ans = {1};
+  for (auto i : mp)
+  {
+    ans *= (i.second + 1);
+  }
+
+  cout << (ans - 1).x << endl;
+
+  return 0;
+}
