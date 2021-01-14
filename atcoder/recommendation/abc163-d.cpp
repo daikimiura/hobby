f38753b3fc8998cc24ac76d94761bfdb129ef097
daikimiura <daikiii5555@gmail.com>
2021-01-14 23:32:41 +0900

    Add abc163 d

---
+++ atcoder/recommendation/abc163-d.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repi(i, a, b) for (int i = int(a); i < int(b); ++i)
+typedef long long ll;
+long long mod = 1e9 + 7;
+const int iINF = 0x3f3f3f3f;
+const long long INF = 1LL << 60;
+using namespace std;
+using pint = pair<int, int>;
+
+template <class T>
+inline bool chmin(T &a, T b)
+{
+  if (a > b)
+  {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+inline bool chmax(T &a, T b)
+{
+  if (a < b)
+  {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+struct mint
+{
+  ll x; // typedef long long ll;
+  mint(ll x = 0) : x((x % mod + mod) % mod) {}
+  mint operator-() const { return mint(-x); }
+  mint &operator+=(const mint a)
+  {
+    if ((x += a.x) >= mod)
+      x -= mod;
+    return *this;
+  }
+  mint &operator-=(const mint a)
+  {
+    if ((x += mod - a.x) >= mod)
+      x -= mod;
+    return *this;
+  }
+  mint &operator*=(const mint a)
+  {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const { return mint(*this) += a; }
+  mint operator-(const mint a) const { return mint(*this) -= a; }
+  mint operator*(const mint a) const { return mint(*this) *= a; }
+  mint pow(ll t) const
+  {
+    if (!t)
+      return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1)
+      a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod - 2); }
+  mint &operator/=(const mint a) { return *this *= a.inv(); }
+  mint operator/(const mint a) const { return mint(*this) /= a; }
+};
+
+struct combination
+{
+  vector<mint> fact, ifact;
+  combination(int n) : fact(n + 1), ifact(n + 1)
+  {
+    assert(n < mod);
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i)
+      fact[i] = fact[i - 1] * i;
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i)
+      ifact[i - 1] = ifact[i] * i;
+  }
+  mint operator()(int n, int k)
+  {
+    if (k < 0 || k > n)
+      return 0;
+    return fact[n] * ifact[k] * ifact[n - k];
+  }
+} c(2000005);
+
+// 100C5 = c(100, 5)
+// 100P5 = c(100,5) * c(100, 5).fact[5]
+
+int main()
+{
+  ll n, k;
+  cin >> n >> k;
+
+  mint ans = 0, min = n * (n + 1) / 2, max = n * (n + 1) / 2;
+
+  for (int i = n + 1; i >= k; i--)
+  {
+    ans += max - min + 1;
+    min -= i - 1;
+    max -= n + 1 - i;
+  }
+
+  cout << ans.x << endl;
+  return 0;
+}
